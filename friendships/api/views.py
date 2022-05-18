4cf5d44da01a85062592f8d8971094a9de4f63fd
hehe7318 <he.xua@northeastern.edu>
2022-05-18 14:16:00 -0700

    initial newsfeeds models api and unit tests

--- friendships/api/views.py
+++ friendships/api/views.py
@@ -17,7 +17,6 @@ class FriendshipViewSet(viewsets.GenericViewSet):
     # 如果是 Friendship.objects.all 的话就会出现 404 Not Found
     # 因为 detail=True 的 actions 会默认先去调用 get_object() 也就是
     # queryset.filter(pk=1) 查询一下这个 object 在不在
-    serializer_class = FriendshipSerializerForCreate
     queryset = User.objects.all()
 
     @action(methods=['GET'], detail=True, permission_classes=[AllowAny])
